ad623812a9f03d91042416e3c1b1071163a955ab
Ricardo Hernandez <ricardo.hernandez.esp@gmail.com>
2023-06-15 01:33:25 -0400

    carpeta sys agregada

--- application/views/front_end/widgets/informaciones.php
+++
@@ -1,9 +0,0 @@
-<?php  
-if(FALSE!=$informaciones){
-	foreach($informaciones as $inf){
-		?>
-			<li class="newsticker__item"><a href="#" class="newsticker__item-url"><?php echo $inf["titulo"] ?></a></li>
-		<?php
-	}
-}
-?>
--- application/views/plantillas/plantilla_admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->load->view('plantillas/admin/header');
-$this->load->view('admin/' . $contenido);
-$this->load->view('plantillas/admin/footer');
-
